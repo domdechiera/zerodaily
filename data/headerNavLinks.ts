9f814ac4a3884ff85914cf43563aa4be0aca7d78
Dom De Chiera <dom@dechiera.com>
2025-04-25 02:27:48 +0800

    fix(blog): tag URLs in header nav links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/tags/data-breach', title: 'Data Breaches' },
-  { href: '/tags/cyber-attacks', title: 'Cyber Attacks' },
-  { href: '/tags/vulnerabilities', title: 'Vulnerabilities' },
+  { href: '/tags/cyber-attack', title: 'Cyber Attacks' },
+  { href: '/tags/vulnerability', title: 'Vulnerabilities' },
   { href: '/resources', title: 'Resources' },
   { href: '/about', title: 'About' },
 ]
