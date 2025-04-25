d761e02075637479563cce270de93b4f7cb443a7
Dom De Chiera <dom@dechiera.com>
2025-04-25 11:32:57 +0800

    chore: update robots.txt to disallow tags/static

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,19 @@
 import { MetadataRoute } from 'next'
 import siteMetadata from '@/data/siteMetadata'
+import { robotsDisallow } from '@/data/robotsDisallow'
 
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
+  // Generate rules: allow everything, but disallow specific paths
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: robotsDisallow,
+      },
+    ],
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
     host: siteMetadata.siteUrl,
   }
---
+++ data/robotsDisallow.ts
@@ -0,0 +1,2 @@
+// List of disallowed paths for robots.txt, with type safety
+export const robotsDisallow: string[] = ['/tags/', '/static/']
