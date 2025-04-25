30f590319f3acaee1e522c4c7535c6ab81bf4721
Dom De Chiera <dom@dechiera.com>
2025-04-25 11:57:20 +0800

    chore: add /projects/ to robots.txt disallow

--- data/robotsDisallow.ts
+++ data/robotsDisallow.ts
@@ -1,2 +1,2 @@
 // List of disallowed paths for robots.txt, with type safety
-export const robotsDisallow: string[] = ['/tags/', '/static/', '/scripts/']
+export const robotsDisallow: string[] = ['/tags/', '/static/', '/scripts/', '/projects/']
