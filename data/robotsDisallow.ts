35411b77e58ac0f393624fb33808c4e22352a621
Dom De Chiera <dom@dechiera.com>
2025-04-25 11:47:15 +0800

    chore: update robots.txt

--- data/robotsDisallow.ts
+++ data/robotsDisallow.ts
@@ -1,2 +1,2 @@
 // List of disallowed paths for robots.txt, with type safety
-export const robotsDisallow: string[] = ['/tags/', '/static/']
+export const robotsDisallow: string[] = ['/tags/', '/static/', '/scripts/']
